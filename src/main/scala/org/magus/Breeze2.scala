2aa5cb3bbce9ba6f1620eb4a8bf04e382ecd3709
ly <1095305853@qq.com>
2021-06-04 17:30:09 +0800

    单体电压一致性算法

--- src/main/scala/org/magus/Breeze2.scala
+++ src/main/scala/org/magus/Breeze2.scala
@@ -18,11 +18,21 @@ object Breeze2 {
     //先创建一个2*92的矩阵(2行，92列)
     val m = DenseMatrix((2,3),(3,5))
     println(m)
-//    for( line <- file.getLines()){
-//
-//      println(line)
-//    }
+    var first = Array(String)
+    for( line <- file.getLines()){
+      val list = line.split(",").toArray
+      if(first == null){
+        first = list
+      }else{
+        val dm1 = DenseMatrix(first)
+        val dm2 = DenseMatrix(list)
 
+      }
+
+
+
+      println(line)
+    }
 
 
 
